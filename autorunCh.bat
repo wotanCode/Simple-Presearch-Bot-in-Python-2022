a89ef3024f6b38622086ea3ed0b14c7cdccbdf7e
Pedro Yanez <pedro_y91@outlook.com>
2022-06-21 21:05:58 -0400

    add autorun bat file and update readme

---
+++ autorunCh.bat
@@ -0,0 +1,4 @@
+echo "RUN PRESARCH BOT FOR CHROME"
+echo "=================================================="
+@echo off
+"C:\Users\pedro\AppData\Local\Programs\Python\Python310\python.exe" "C:\Users\pedro\OneDrive\Escritorio\presearch-bot\presearch_ch.py"
